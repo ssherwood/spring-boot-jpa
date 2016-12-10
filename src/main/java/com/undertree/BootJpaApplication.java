4ac2f3b2ebb046f093443ee3b6737285895a5260
Shawn Sherwood <shawn.sherwood@gmail.com>
2016-12-10 08:58:57 -0500

    Initial setup

---
+++ src/main/java/com/undertree/BootJpaApplication.java
@@ -0,0 +1,12 @@
+package com.undertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaApplication.class, args);
+	}
+}

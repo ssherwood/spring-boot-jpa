3f063f8f013571fc9e8012125e1fb8517031b36b
Shawn Sherwood <ssherwood@pivotal.io>
2018-08-11 13:18:17 -0400

    Additional minor cleanup after upgrade

--- src/main/java/io/undertree/symptom/domain/Medication.java
+++ src/main/java/io/undertree/symptom/domain/Medication.java
@@ -20,11 +20,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * See also:

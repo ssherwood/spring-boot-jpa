4e644b4dc1d8a9d28a5ef7fd922e7f5cf22f5ba4
Shawn Sherwood <ssherwood@pivotal.io>
2018-10-05 15:25:07 -0400

    Fixing regex

--- pipeline/scripts/app-info.sh
+++ pipeline/scripts/app-info.sh
@@ -17,7 +17,7 @@ cf apps
 set +ex
 
 # Get app colour
-CURR_APP_COLOUR=$(cf apps | grep "$CF_APP.$CF_APP_DOMAIN" | awk '{print $1}')
+CURR_APP_COLOUR=$(cf apps | grep "${CF_APP}\-.*\.${CF_APP_DOMAIN}" | awk '{print $1}')
 if [[ $CURR_APP_COLOUR =~ .*green.* ]]; then
   echo "green" > ./app-info/current-app.txt
   echo "blue" > ./app-info/next-deployment.txt
